38ff85d669de28be3f371c87bd1e5addc2bbe557
Katerina <katerina@as-MacBook-Pro.local>
2020-05-12 19:28:28 +0300

    add person prototype realisation

---
+++ person.js
@@ -0,0 +1,17 @@
+function Person(name) {
+  this.name = name;
+}
+
+const juan = new Person('Juan');
+
+Person.prototype = {
+  getName: function () {
+    return this.name;
+  }
+};
+
+const pedro = new Person('Pedro');
+// const juan = new Person('Juan');
+
+console.log(pedro.getName());
+console.log(juan.getName());
